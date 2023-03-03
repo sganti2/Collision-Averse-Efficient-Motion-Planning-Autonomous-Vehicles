c728add9a7313ccee69d70e3257dc1dfb268f1fe
Sruti Ganti <sganti2@asu.edu>
2023-03-03 13:10:02 -0700

    Corrected Case1

--- Single-Agent/Case1/examples/2D_Motion_Planner_Case1.py
+++ Single-Agent/Case1/examples/2D_Motion_Planner_Case1.py
@@ -20,35 +20,39 @@
 from omgtools import *
 
 # create vehicle
-vehicle = Holonomic()
-vehicle.set_options({'safety_distance': 0.1})
-vehicle.set_options({'ideal_prediction': False})
+vehicle = Bicycle(length=0.4, options={'plot_type': 'car', 'substitution': False})
+vehicle.define_knots(knot_intervals=5)  # choose lower amount of knot intervals
 
-vehicle.set_initial_conditions([-1.5, -1.5])
-vehicle.set_terminal_conditions([2., 2.])
+vehicle.set_initial_conditions([0., 0., 0., 0.])  # x, y, theta, delta
+vehicle.set_terminal_conditions([3., 3., 0.])  # x, y, theta
 
 # create environment
-environment = Environment(room={'shape': Square(5.)})
-rectangle = Rectangle(width=3., height=0.2)
-
-# environment.add_obstacle(Obstacle({'position': [-2.1, -0.5]}, shape=rectangle))
-# environment.add_obstacle(Obstacle({'position': [1.7, -0.5]}, shape=rectangle))
-trajectories = {'velocity': {'time': [0., 40.],
-                             'values': [[0., 0.], [0., 0.]]}}
-environment.add_obstacle(Obstacle({'position': [0, 0]}, shape=Circle(0.8), options={'bounce':False},
+environment = Environment(room={'shape': Square(5.), 'position': [1.5, 1.5]})
+trajectories = {'velocity': {'time': [0.5],
+                             'values': [[0.0, 0.0]]}}
+environment.add_obstacle(Obstacle({'position': [1., 1.]}, shape=Circle(0.5),
                                   simulation={'trajectories': trajectories}))
 
 # create a point-to-point problem
-problem = Point2point(vehicle, environment, freeT=False)
+problem = Point2point(vehicle, environment, freeT=True)
+# extra solver settings which may improve performance
+#problem.set_options({'solver_options': {'ipopt': {'ipopt.linear_solver': 'ma57'}}})
 problem.init()
+
+vehicle.problem = problem  # to plot error if using substitution
 
 # create simulator
 simulator = Simulator(problem)
+
 problem.plot('scene')
-vehicle.plot('input', knots=True, prediction=True, labels=['v_x (m/s)', 'v_y (m/s)'])
+vehicle.plot('input', knots=True, labels=['v (m/s)', 'ddelta (rad/s)'])
+vehicle.plot('state', knots=True, labels=[
+             'x (m)', 'y (m)', 'theta (rad)', 'delta (rad)'])
+if vehicle.options['substitution']:
+	vehicle.plot('err_pos', knots=True)
+	vehicle.plot('err_dpos', knots=True)
 
 # run it!
 simulator.run()
 
 problem.save_movie('scene', format='gif', name='problemgif', number_of_frames=80, movie_time=4, axis=False)
-
